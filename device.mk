fef6eef4a2dc36deaf1f2e6b27549f218dc27108
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-11-12 19:29:06 -0200

    kenzo: Rootdir: Introduce 'SafailNet'
    * Temporary safetynet haxx until we gets enforcing mode
    
    I don't know why I picked it
    
    Signed-off-by: jhenrique09 <jhenrique09.mcz@hotmail.com>
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>

--- device.mk
+++ device.mk
@@ -352,7 +352,8 @@ PRODUCT_PACKAGES += \
     init.qcom.rc \
     init.target.rc \
     init.qcom.usb.rc \
-    ueventd.qcom.rc
+    ueventd.qcom.rc \
+    init.safailnet.rc
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \
