02f0749664aac5ad694aa065deda4808d5c664de
Jeff Tinker <jtinker@google.com>
2019-10-19 23:31:30 +0530

    kenzo: Enable clearkey drm plugin v1.2
    test: android.media.cts.MediaDrmClearkeyTest#testOfflineKeyManagement
    
    bug:117570956
    bug:110838441
    bug:116252891
    Change-Id: Ie9373543ef416e718a70e75d95bf5b15a2852705
    
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>

--- device.mk
+++ device.mk
@@ -180,7 +180,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.1-service.clearkey
+    android.hardware.drm@1.2-service.clearkey
 
 #Face  detection extension
 PRODUCT_PACKAGES += \
