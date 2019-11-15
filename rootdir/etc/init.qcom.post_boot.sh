cd6c63b53d05dc2bb3160f4c660cc2d8117d9b54
ARSLҽʋαɳƚҽιɳ <kiritoclasher@gmail.com>
2019-11-15 23:49:22 +0700

    kenzo: Add earpiece and speaker gain values
    Set Sound Control parameters after boot

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -243,3 +243,7 @@ setprop persist.vendor.mmi.misc_dev_path $real_path
 
 # Set BFQ as default io-schedular after boot
 setprop sys.io.scheduler "cfq"
+
+# Set Sound Control parameters after boot
+echo 5 > /sys/kernel/sound_control/speaker_gain
+echo "3 3" > /sys/kernel/sound_control/earpiece_gain
