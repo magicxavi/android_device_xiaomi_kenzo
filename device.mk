ec0ab59bdcb75b5003eb149096deb1db00a03d74
Kyryll Bondarenko <rolfpeterkesche@googlemail.com>
2019-11-21 20:29:24 +0200

    kenzo: build power stats hal
    Cannot find entry android.hardware.power.stats@1.0::IPowerStats/default in either framework or device manifest.

--- device.mk
+++ device.mk
@@ -346,7 +346,8 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
+    android.hardware.power@1.2-service-qti \
+    android.hardware.power.stats@1.0-service.mock
 
 # Qualcomm dependencies
 PRODUCT_PACKAGES += \
