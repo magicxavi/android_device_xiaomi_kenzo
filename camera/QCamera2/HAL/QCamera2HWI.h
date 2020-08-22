1bf5e4741de96e1e93760ce1c607e9380a7b3f90
Magicxavi <rolfpeterkesche@gmail.com>
2020-08-22 06:12:25 +0300

    kenzo: fix build errors after camera import

--- camera/QCamera2/HAL/QCamera2HWI.h
+++ camera/QCamera2/HAL/QCamera2HWI.h
@@ -35,7 +35,7 @@
 #include <utils/Log.h>
 #include <utils/Mutex.h>
 #include <utils/Condition.h>
-#include <QCameraParameters.h>
+#include "QCameraParameters.h"
 
 #include "QCameraTrace.h"
 #include "QCameraQueue.h"
