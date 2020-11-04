43125162848cd838e8a8db5f85f847ff2b39a5eb
Magicxavi <rolfpeterkesche@gmail.com>
2020-11-04 08:21:01 +0200

    kenzo: power off alarm

--- device.mk
+++ device.mk
@@ -372,6 +372,7 @@ PRODUCT_PACKAGES += \
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \
+    init.qcom.early_boot.sh \
     init.qcom.sh \
     init.qcom.post_boot.sh
 
--- init/init_msm.cpp
+++ init/init_msm.cpp
@@ -27,8 +27,18 @@
 
 #include <sys/sysinfo.h>
 
-#include "property_service.h"
+#include <cstdio>
 
+#include <android-base/file.h>
+#include <android-base/properties.h>
+#include <android-base/strings.h>
+
+#include "property_service.h"
+#include "vendor_init.h"
+
+using android::base::GetProperty;
+using android::base::ReadFileToString;
+using android::base::Trim;
 using android::init::property_set;
 
 char const *heapstartsize;
@@ -60,8 +70,41 @@ void check_device()
     }
 }
 
+static void init_alarm_boot_properties()
+{
+    char const *boot_reason_file = "/proc/sys/kernel/boot_reason";
+    char const *power_off_alarm_file = "/persist/alarm/powerOffAlarmSet";
+    std::string boot_reason;
+    std::string power_off_alarm;
+    std::string reboot_reason = GetProperty("ro.boot.alarmboot", "");
+    if (ReadFileToString(boot_reason_file, &boot_reason)
+            && ReadFileToString(power_off_alarm_file, &power_off_alarm)) {
+        /*
+         * Setup ro.alarm_boot value to true when it is RTC triggered boot up
+         * For existing PMIC chips, the following mapping applies
+         * for the value of boot_reason:
+         *
+         * 0 -> unknown
+         * 1 -> hard reset
+         * 2 -> sudden momentary power loss (SMPL)
+         * 3 -> real time clock (RTC)
+         * 4 -> DC charger inserted
+         * 5 -> USB charger inserted
+         * 6 -> PON1 pin toggled (for secondary PMICs)
+         * 7 -> CBLPWR_N pin toggled (for external power supply)
+         * 8 -> KPDPWR_N pin toggled (power key pressed)
+         */
+        if ((Trim(boot_reason) == "3" || reboot_reason == "true")
+                && Trim(power_off_alarm) == "1")
+            property_set("ro.alarm_boot", "true");
+        else
+            property_set("ro.alarm_boot", "false");
+    }
+}
+
 void vendor_load_properties()
 {
+    init_alarm_boot_properties();
     check_device();
 
     property_set("dalvik.vm.heapstartsize", heapstartsize);
