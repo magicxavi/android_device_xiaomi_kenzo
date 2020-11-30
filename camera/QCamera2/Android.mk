4b87a8d77ac02e59da638ec7466df1db0cf96c18
Scott Warner <Tortel1210@gmail.com>
2020-11-30 12:06:27 -0500

    kenzo: camera: Depend on display_headers
    Since gralloc_priv.h is no longer copied, include display_headers
    to pick it up again
    
    Change-Id: I607037ad1a16276d63228378f4e5aee3b4dc3b7a

--- camera/QCamera2/Android.mk
+++ camera/QCamera2/Android.mk
@@ -96,6 +96,7 @@ ifeq ($(TARGET_TS_MAKEUP),true)
 LOCAL_SHARED_LIBRARIES += libts_face_beautify_hal libts_detected_face_hal
 endif
 LOCAL_HEADER_LIBRARIES += camera_common_headers
+LOCAL_HEADER_LIBRARIES += display_headers
 
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_PROPRIETARY_MODULE := true
