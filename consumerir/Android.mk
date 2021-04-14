4e8c69aead540ea3552ffb0598d8e5d72f053ae9
Demon <demonsingur@gmail.com>
2021-04-15 02:19:30 +0300

    kenzo: ir: Convert IR HAL into a native binderized HAL
    Change-Id: I8ddae1ba36d5a9cc16f0937189ff250eff3e7935

--- consumerir/Android.mk
+++ consumerir/Android.mk
@@ -1,28 +1,23 @@
-# Copyright (C) 2013 The Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-
-LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
-LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
-
-LOCAL_MODULE := consumerir.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE := android.hardware.ir@1.0-service-custom
+LOCAL_INIT_RC := android.hardware.ir@1.0-service.rc
+LOCAL_PROPRIETARY_MODULE := true
 LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_SRC_FILES := consumerir.c
-LOCAL_SHARED_LIBRARIES := liblog libcutils
-LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := \
+    service.cpp \
+    ConsumerIr.cpp
 
-include $(BUILD_SHARED_LIBRARY)
+LOCAL_SHARED_LIBRARIES := \
+    libbase \
+    libhardware \
+    libhidlbase \
+    libhidltransport \
+    libhwbinder \
+    libutils \
+    android.hardware.ir@1.0
+
+include $(BUILD_EXECUTABLE)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
