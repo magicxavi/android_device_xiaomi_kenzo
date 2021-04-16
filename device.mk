22215dea24856344a0dbbf9aa34743b175da5c53
Magicxavi <rolfpeterkesche@gmail.com>
2021-04-16 16:22:45 +0300

    kenzo: kang Widevine DRM 1.2 blobs from daisy
    daisy_sprout:10/QKQ1.191002.002/V11.0.18.0.QDLMIXM

--- device.mk
+++ device.mk
@@ -186,7 +186,7 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.2-service.clearkey \
-    android.hardware.drm@1.0-service.widevine
+    android.hardware.drm@1.2-service.widevine
 
 #Face  detection extension
 PRODUCT_PACKAGES += \
