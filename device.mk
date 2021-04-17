0c68132eda7cdf90f882215bbdd08b4d6fef3f40
timibreakdown <timifix@gmail.com>
2021-04-17 04:18:12 +0300

    Revert "kenzo: build libwifi-hal-ctrl"
    This reverts commit cf0af932cd97dceb7d284f7ccbd418e8bfe1bd2e.

--- device.mk
+++ device.mk
@@ -526,8 +526,7 @@ PRODUCT_PACKAGES += \
     hostapd \
     wificond \
     wpa_supplicant \
-    wpa_supplicant.conf \
-    libwifi-hal-ctrl
+    wpa_supplicant.conf
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
