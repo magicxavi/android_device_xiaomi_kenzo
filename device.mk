b8d5e5e9c5bdf0f079e564898351e7814c467c5e
Arian <arian.kulmer@web.de>
2021-07-02 17:27:13 +0300

    kenzo: Build new aidl qti power HAL
    - CAF converted their hidl power hal to aidl
    
    Change-Id: I4bdea354e54d34d0dfe9b811e228fc6f8762d2ed

--- device.mk
+++ device.mk
@@ -356,8 +356,7 @@ endif
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti \
-    android.hardware.power.stats@1.0-service.mock
+    android.hardware.power-service-qti
 
 # Qualcomm dependencies
 PRODUCT_PACKAGES += \
