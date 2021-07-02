c6c16be589acc492711707d65d6d06da12d98dfc
ix5 <ix5@users.noreply.github.com>
2021-07-02 18:36:07 +0300

    kenzo: Add protobuf vendorcompat lib
    Our blobs are compiled against libprotobuf-full-cpp.so, but
    starting in R, Android is using a versioned naming approach,
    e.g. libprotobuf-cpp-full-3.9.1.so.
    
    See https://r.android.com/1109518
    
    The entry in public.libraries.txt is needed to allow the
    linker to find and use the lib outside the VNDK.
    
    See https://source.android.com/devices/tech/config/namespaces_libraries
    
    [wight554: added libprotobuf-cpp-lite as well]
    
    Signed-off-by: Volodymyr Zhdanov <wight554@gmail.com>
    Change-Id: I28222c89e1e07017867088492a726ee9d45d8e6f

--- device.mk
+++ device.mk
@@ -68,6 +68,10 @@ PRODUCT_PACKAGES += \
    libandroid_net \
    libandroid_net_32
 
+# Additional native libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService \
