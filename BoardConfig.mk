56a4b57785712c4d9eb54ff1600dbc2ef0f8f9bb
Michael Bestas <mkbestas@lineageos.org>
2021-09-03 15:46:41 +0300

    kenzo: Set androidboot.init_fatal_reboot_target=recovery
    * Don't crash to bootloader, users can access pstore in
      recovery in order to debug the issue.
    
    Change-Id: I6025b6af74e8052918c101236a46cb512a888695

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -166,6 +166,7 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_msm
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 ramoops_memreserve=4M
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware_mnt/image
+BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
