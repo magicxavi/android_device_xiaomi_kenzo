f176e2a5057535caba0e81303d7ba15bf17fb9d5
Magicxavi <rolfpeterkesche@gmail.com>
2021-10-13 14:33:28 +0300

    Revert "kenzo: build enforcing"
    This reverts commit 8abdce435194f2f22f2cfb9a9a998debc6904f2d.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -167,6 +167,7 @@ BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom ehci-hcd.park=3 androidboot.bo
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware_mnt/image
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
