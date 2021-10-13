98946664fcbfee25c60b40fa39e0fd33e675db67
Magicxavi <rolfpeterkesche@gmail.com>
2021-10-13 15:32:48 +0300

    kenzo: build keymaster 4.0

--- device.mk
+++ device.mk
@@ -277,7 +277,9 @@ PRODUCT_COPY_FILES += \
 # Keymaster HAL
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service
+    android.hardware.keymaster@3.0-service \
+    android.hardware.keymaster@4.0-impl \
+    android.hardware.keymaster@4.0-service
 
 # Lights
 PRODUCT_PACKAGES += \
