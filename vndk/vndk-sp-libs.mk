93c7b6757c34c38e4d3d599d5d3fe9f4ac57c528
Magicxavi <rolfpeterkesche@gmail.com>
2021-10-13 14:59:50 +0300

    kenzo: drop libdexfile_support

--- vndk/vndk-sp-libs.mk
+++ vndk/vndk-sp-libs.mk
@@ -3,7 +3,6 @@ VNDK_SP_LIBRARIES := \
     android.hardware.graphics.allocator@2.0 \
     android.hardware.graphics.allocator@3.0 \
     libbinder \
-    libdexfile_support \
     libui
 
 install_in_hw_dir := \
