1327c33399227ac3b880444051b0772967a0f8fa
Magicxavi <rolfpeterkesche@gmail.com>
2022-03-30 01:15:06 +0300

    kenzo: use camera go by default

--- device.mk
+++ device.mk
@@ -135,9 +135,7 @@ PRODUCT_PACKAGES += \
     camera.device@3.2-impl \
     camera.msm8952 \
     libqomx_core \
-    Snap \
-    SnapdragonCamera \
-    SnapdragonCamera2 \
+    Camera_Go \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
 
